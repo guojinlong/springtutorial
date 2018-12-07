8ee62bc8c9fa74556ba2a893084bc3d91df11271
root <root@DESKTOP-1634O8V.localdomain>
2018-12-07 18:31:53 +0800

    新增 忽略文件

--- springbatch/src/main/java/xyz/guojinlong/batch/SpringBatchApplication.java
+++ springbatch/src/main/java/xyz/guojinlong/batch/SpringBatchApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
